619463eacfd0174f4bea7086b501aa6408df7903
Owen <Floul1@hotmail.co.uk>
2017-10-10 18:03:11 +0100

    So long farewell
    Auf wiedersehen goodbye

---
+++ WebApiCore/Controllers/ApiControllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WhiteHingeFrameworkExt.Models;
+
+namespace WebApiCore.Controllers.ApiControllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Produces("application/json")]
+    public class OrderController : Controller
+    {
+        /// <summary>
+        /// Test App
+        /// </summary>
+        /// <returns>A list of all device data</returns>
+        [HttpPost]
+        [Route("Test/Logs")]
+        public List<AndroidDevices> GetAllLog()
+        {
+            var returnable = new List<AndroidDevices>();
+            using (var context = new WhiteHingeContext())
+            {
+                returnable.AddRange(context.AndroidDeviceLog);
+            }
+            return returnable;
+        }
+    }
+}
